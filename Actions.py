06303bacfdad82c10dcb9be9d67569e34d196759
mlkmhmtbyk <biyikm16@itu.edu.tr>
2019-12-23 05:25:07 +0300

    Contact functions added

--- Actions.py
+++ Actions.py
@@ -16,15 +16,26 @@ def SearchEvent(eventName):
         if (i.__contains__(eventName)):
             print(i)   	
 
-def DeleteEvent(eventName):
-	f = open("events.txt", "r+")
-	f1 = f.readlines()
-	for i in f1:
-		if (i.__contains__(eventName)):
-			f.truncate(i)  
+#def DeleteEvent(eventName):
+#	f = open("events.txt", "r+")
+#	f1 = f.readlines()
+#	for i in f1:
+#		if (i.__contains__(eventName)):
+#			f.
+#	f.close()
+
+def GetContatcts() :
+	f = open("contacts.txt", "r")
+	contents =f.read()
+	print(contents)
 	f.close()
 
+def GetContact(contact):
+	f = open("contacts.txt", "r")
+	f1 = f.readlines()
+	for i in f1:
+		if (i.__contains__(contact)):
+			print(i)  
 
-DeleteEvent("Kahvaltı")
-
+		
 input('Press ENTER to exit')
